838dd6578b57b00573cb53633008907c38347240
Hack Reactor Student <student@hackreactor.com>
2018-02-09 16:46:42 -0800

    set up our tables and connection..?

--- server/db/index.js
+++ server/db/index.js
@@ -3,5 +3,16 @@ var mysql = require('mysql');
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
-
-
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'chat'
+});
+connection.connect((err) => {
+  if (err) {
+      console.log('Error connecting...');
+      return;
+  }
+  console.log('Connected!');
+});
--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,28 @@
-CREATE DATABASE chat;
+-- CREATE DATABASE chat;
 
 USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  users_id INT,
+  room VARCHAR(20),
+  message VARCHAR(140)
 );
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(20)
+);
 
-
+CREATE TABLE rooms (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(20)
+);
 
 
 /*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
+ *    mysql -u student < server/schema.sql
  *  to create the database and the tables.*/
 
